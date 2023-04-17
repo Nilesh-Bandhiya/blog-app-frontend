c4981b507ab790648ad4f2bd03cd05fba69ce20e
Radhe5050 <radheayar5050@gmail.com>
2023-04-17 18:21:18 +0530

    changes in route

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,8 @@
 export const BLOG_APP_BASE_URL = process.env.REACT_APP_BASE_URL || '';
 
 export const APIS = {
-    USERS_API: `${BLOG_APP_BASE_URL}/users`,
-    BLOGS_API: `${BLOG_APP_BASE_URL}/blogs`
+    USERS_API: `${BLOG_APP_BASE_URL}/api/users`,
+    BLOGS_API: `${BLOG_APP_BASE_URL}/api/blogs`
 };
 
 export const paginationPageSize = 7
