52d7202879c82245120308720f6fe10126b596a3
Radhe5050 <radheayar5050@gmail.com>
2023-09-04 11:52:06 +0530

    refresh token issue fixed

--- src/services/axiosServices/axiosInterceptors.js
+++ src/services/axiosServices/axiosInterceptors.js
@@ -5,58 +5,64 @@ import { getRefreshToken } from "../api/usersApi";
 export const axiosInstance = axios.create();
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = getLocalAccessToken();
-        if (token) {
-            config.headers['Authorization'] = 'Bearer ' + token;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = getLocalAccessToken();
+    if (token) {
+      config.headers["Authorization"] = "Bearer " + token;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 axiosInstance.interceptors.response.use(
-    (res) => {
-        return res;
-    },
-    async (err) => {
-        const originalConfig = err.config;
-        if (err.response) {
-            // Access Token was expired
-            if (err.response.status === 419 && !originalConfig._retry) {
-                originalConfig._retry = true;
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response) {
+      // Access Token was expired
+      if (err.response.status === 419 && !originalConfig._retry) {
+        originalConfig._retry = true;
 
-                try {
-                    const rs = await getRefreshToken();
-                    const { token } = rs.data;
-                    localStorage.setItem("token", JSON.stringify(token));
-                    axiosInstance.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+        try {
+          const rs = await getRefreshToken();
+          const { token } = rs.data;
+          localStorage.setItem("token", JSON.stringify(token));
+          axiosInstance.defaults.headers.common["Authorization"] =
+            "Bearer " + token;
 
-                    return axiosInstance(originalConfig);
-                } catch (_error) {
-                    if (_error.response && _error.response.data) {
-                        return Promise.reject(_error.response.data);
-                    }
+          return axiosInstance(originalConfig);
+        } catch (_error) {
+          if (_error.response && _error.response.data) {
+            localStorage.removeItem("refreshToken");
+            localStorage.removeItem("userData");
+            localStorage.removeItem("token");
+            // eslint-disable-next-line no-restricted-globals
+            location.href = "/signin";
+            return Promise.reject(_error.response.data);
+          }
 
-                    return Promise.reject(_error);
-                }
-            }
-
-            if (err.response.status === 420 && !originalConfig._retry) {
-                localStorage.removeItem("refreshToken");
-                localStorage.removeItem("userData");
-                localStorage.removeItem("token");
-                // eslint-disable-next-line no-restricted-globals
-                location.href = '/signin'
-            }
-
-            if (err.response.status === 403 && err.response.data) {
-                return Promise.reject(err.response.data);
-            }
+          return Promise.reject(_error);
         }
+      }
 
-        return Promise.reject(err);
+      if (err.response.status === 420 && !originalConfig._retry) {
+        localStorage.removeItem("refreshToken");
+        localStorage.removeItem("userData");
+        localStorage.removeItem("token");
+        // eslint-disable-next-line no-restricted-globals
+        location.href = "/signin";
+      }
+
+      if (err.response.status === 403 && err.response.data) {
+        return Promise.reject(err.response.data);
+      }
     }
+
+    return Promise.reject(err);
+  }
 );
